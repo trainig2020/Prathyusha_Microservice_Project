ce11a097ef25c0816efed34f86b5184a3cde5250
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-09-04 20:42:31 +0530

    Add files via upload

---
+++ DepEmpMvc/src/main/java/io/prathyusha/depempmvc/config/Interceptor.java
@@ -0,0 +1,48 @@
+package io.prathyusha.depempmvc.config;
+
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component
+public class Interceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		
+		Calendar cal = Calendar.getInstance();
+
+		int dayOfWeek = cal.get(cal.DAY_OF_WEEK);
+		if (dayOfWeek == 1) {
+			response.getWriter().write("The website is closed on sunday; please try accessing it" + "on any ather week day!!");
+			return false;
+		}
+		return true;	
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("HandlerInterceptorAdapter : Spring MVC called postHandle method for "+
+			request.getRequestURI().toString());
+	
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		
+		System.out.println("HandlerInterceptorAdapter : Spring MVC called aftercompletion method for "
+				+ request.getRequestURI().toString());
+
+			
+	}
+	
+	
+
+}
